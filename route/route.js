f36d6732ed74a09a959a575ffcbb265f37e983d2
walkingcabbage <aksdn1513@gmail.com>
2022-12-23 17:11:31 +0900

    ejs연결 및 css 수정 (40% / 100%)

--- route/route.js
+++ route/route.js
@@ -3,13 +3,60 @@ const router = express.Router();
 
 
 router.get('/', function (req, res) {
-  res.render('index',{subHeader:false});
+  res.render('index', {
+    subHeader: false,
+    subFooter:false,
+    category:'main',
+    subcategory:'main',
+  });
+})
+router.get('/introPage', function (req, res) {
+  res.render('intro', {
+    subHeader: true,
+    subFooter:true,
+    category:'intro',
+    subcategory:'intro',
+  });
 })
 router.get('/expPage', function (req, res) {
-  res.render('experience',{subHeader:true});
+  res.render('experience', {
+    subHeader: true,
+    subFooter:true,
+    category:'program',
+    subcategory:'exp',
+  });
 })
 router.get('/campPage', function (req, res) {
-  res.render('camp',{subHeader:true});
+  res.render('camp', {
+    subHeader: true,
+    subFooter:true,
+    category:'program',
+    subcategory:'camp',
+  });
+})
+router.get('/lodgingPage', function (req, res) {
+  res.render('lodging', {
+    subHeader: true,
+    subFooter:true,
+    category:'facility',
+    subcategory:'lodging',
+  });
+})
+router.get('/auditoriumPage', function (req, res) {
+  res.render('auditorium', {
+    subHeader: true,
+    subFooter:true,
+    category:'facility',
+    subcategory:'auditorium',
+  });
+})
+router.get('/expcenterPage', function (req, res) {
+  res.render('expcenter', {
+    subHeader: true,
+    subFooter:true,
+    category:'facility',
+    subcategory:'expcenter',
+  });
 })
 
 
