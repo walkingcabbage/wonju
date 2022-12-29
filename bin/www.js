98b18ccad8fecc4d39b226571072299156f6de92
walkingcabbage <aksdn1513@gmail.com>
2022-12-29 17:50:26 +0900

    www,pkg 수정

--- bin/www.js
+++ bin/www.js
@@ -1,7 +1,5 @@
 let app = require('../app');
 
-let port=process.env.PORT||3005;
+let port=3005;
 
-app.listen(port,()=>{
-  console.log(`http://127.0.0.1:${port} 로 express 실행`);
-})
+app.listen(process.env.PORT||port);
