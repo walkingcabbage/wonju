f2931260dff1ce58d9a4c17830b2118551dd8c21
gyeo <104889999+rudns2@users.noreply.github.com>
2022-12-29 09:38:35 +0900

    12.29-09:38

---
+++ db.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+var connection =  mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "1234",
+  database:'memo',
+  multipleStatements: true
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
